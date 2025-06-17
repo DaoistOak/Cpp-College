bffcc0c934c46cd51dc54d963a087a92c2cc2dc2
DaoistOak <shridal99@gmail.com>
2025-06-17 08:08:21 +0545

    Class-complete

--- Day9/Ex2.cpp
+++ Day9/Ex2.cpp
@@ -1,1 +1,27 @@
 #include<iostream>
+using namespace std;
+
+class DistanceConverter {
+    float distance;
+public:
+    DistanceConverter(float d) {
+        distance = d;
+    }
+    float convertToKm() {
+        return distance / 1000;
+    }
+    float convertToMeter() {
+        return distance * 1000;
+    }
+};
+
+int main() {
+    float d;
+    cout << "Enter distance in Meter: ";
+    cin >> d;
+    DistanceConverter dc(d);
+    cout << "Distance in Km: " << dc.convertToKm() << endl;
+    cout << "Distance in Meter: " << dc.convertToMeter() << endl;
+    return 0;
+}
+
